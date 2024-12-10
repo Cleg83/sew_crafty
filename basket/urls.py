4532f19fcc970f6bab10ebf9e3327191fb3f5e20
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-12-10 19:48:43 +0000

    Add update basket view

--- basket/urls.py
+++ basket/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.view_basket, name='view_basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
     path('delete/<item_id>', views.delete_from_basket, name='delete_from_basket'),
+    path('adjust/<item_id>', views.adjust_basket, name='adjust_basket'),
 ]
