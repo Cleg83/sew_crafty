c61745a6400555bdb0e304eda67928d305b7b4d5
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-12-16 20:12:14 +0000

    Customise allauth templates and ensure view is rendering profile info

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 # Create your views here.
 def user_profile(request):
+    """A view to show the user's profile"""
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'user_profile/user_profile.html'
-    context={}
+    context={
+        'profile':profile,
+    }
     return render(request, template, context)
