a6d03d8370daf9ee4fe0c2daf24bb804c4c0b16f
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-12-17 22:54:59 +0000

    Add user to order admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -14,7 +14,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'delivery_fee', 'order_total',
                        'grand_total', 'original_basket', 'stripe_pid',)
     
-    fields = ('order_number', 'date', 'first_name', 
+    fields = ('order_number', 'user_profile', 'date', 'first_name', 
               'last_name', 'email', 'phone_number', 'country', 
               'postcode', 'town', 'address_1', 
               'address_2', 'county', 'item_count', 
